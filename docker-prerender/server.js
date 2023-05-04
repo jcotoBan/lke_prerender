2ea7a41d659333f23590fa3e6ea3c9c5fdf71222
jcotsan <jcotsan@gmail.com>
2023-05-04 15:37:33 +0000

    First cm

---
+++ docker-prerender/server.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+var prerender = require('prerender');
+
+const https = require('https');
+const http = require('http');
+const fs = require('fs');
+
+var server = prerender({
+  chromeFlags: ['--no-sandbox','--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars'],
+  chromeLocation: '/usr/bin/chromium-browser',
+  port: 8080 
+});
+
+server.use(prerender.sendPrerenderHeader());
+server.use(prerender.browserForceRestart());
+// server.use(prerender.blockResources());
+server.use(prerender.addMetaTags());
+server.use(prerender.removeScriptTags());
+server.use(prerender.httpHeaders());
+
+server.use(require('prerender-memory-cache'));
+
+server.start();
