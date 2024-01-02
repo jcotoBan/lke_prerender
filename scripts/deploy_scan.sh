9e9f517105af170165914ed3f9bcf1b995246f79
Juan Coto <jcotsan@gmail.com>
2024-01-02 11:32:34 -0600

    testing actions

--- scripts/deploy_scan.sh
+++ scripts/deploy_scan.sh
@@ -2,6 +2,6 @@
 
 curl -LO "https://download.sysdig.com/scanning/bin/sysdig-cli-scanner/$(curl -L -s https://download.sysdig.com/scanning/sysdig-cli-scanner/latest_version.txt)/linux/amd64/sysdig-cli-scanner"
 chmod +x ./sysdig-cli-scanner
-SECURE_API_TOKEN=sysdig_api ./sysdig-cli-scanner --apiurl https://us2.app.sysdig.com koton00beng/prerender:main
+SECURE_API_TOKEN=$sysdig_api ./sysdig-cli-scanner --apiurl https://us2.app.sysdig.com koton00beng/prerender:main
 
 cat /home/runner/work/lke_prerender/lke_prerender/scan-logs
