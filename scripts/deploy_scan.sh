416eb855220473b17d4448653906e7aa916ea79a
Juan Coto Sanchez <juancarlos.coto@juancarlos.coto>
2024-04-08 23:05:16 -0600

    setting policy to scan

--- scripts/deploy_scan.sh
+++ scripts/deploy_scan.sh
@@ -2,4 +2,4 @@
 
 curl -LO "https://download.sysdig.com/scanning/bin/sysdig-cli-scanner/$(curl -L -s https://download.sysdig.com/scanning/sysdig-cli-scanner/latest_version.txt)/linux/amd64/sysdig-cli-scanner"
 chmod +x ./sysdig-cli-scanner
-SECURE_API_TOKEN=$sysdig_api ./sysdig-cli-scanner --apiurl $sysdig_api_url --console-log koton00beng/prerender:main
+SECURE_API_TOKEN=$sysdig_api ./sysdig-cli-scanner --apiurl $sysdig_api_url --policy coto_log4j --console-log koton00beng/prerender:main
