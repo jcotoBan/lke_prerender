1f7be2c417c902c7d133a3103dd36dbf24e5037f
Juan Coto Sanchez <juancarlos.coto@juancarlos.coto>
2025-03-11 08:54:02 -0600

    testing alma 8 pipeline scan

--- scripts/deploy_scan.sh
+++ scripts/deploy_scan.sh
@@ -2,6 +2,6 @@
 
 curl -LO "https://download.sysdig.com/scanning/bin/sysdig-cli-scanner/$(curl -L -s https://download.sysdig.com/scanning/sysdig-cli-scanner/latest_version.txt)/linux/amd64/sysdig-cli-scanner"
 chmod +x ./sysdig-cli-scanner
-docker pull opensuse/leap:15.6
+docker pull almalinux:8
 #SECURE_API_TOKEN=$sysdig_api ./sysdig-cli-scanner --apiurl $sysdig_api_url --policy coto_log4j --console-log 992382711296.dkr.ecr.us-east-1.amazonaws.com/prerender:main
-SECURE_API_TOKEN=$sysdig_api ./sysdig-cli-scanner --apiurl $sysdig_api_url --policy coto_log4j --console-log opensuse/leap:15.6
+SECURE_API_TOKEN=$sysdig_api ./sysdig-cli-scanner --apiurl $sysdig_api_url --policy coto_log4j --console-log almalinux:8
