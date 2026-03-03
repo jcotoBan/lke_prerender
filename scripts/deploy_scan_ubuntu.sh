bb92cd4598a1126cf6da0eb29563b84670fd28ae
Juan Coto Sanchez <juancarlos.coto@juancarlos.coto>
2026-03-03 17:17:03 -0600

    fixing logic of ubuntu docker tag and script

--- scripts/deploy_scan_ubuntu.sh
+++ scripts/deploy_scan_ubuntu.sh
@@ -2,4 +2,4 @@
 
 curl -LO "https://download.sysdig.com/scanning/bin/sysdig-cli-scanner/$(curl -L -s https://download.sysdig.com/scanning/sysdig-cli-scanner/latest_version.txt)/linux/amd64/sysdig-cli-scanner"
 chmod +x ./sysdig-cli-scanner
-SECURE_API_TOKEN=$sysdig_api ./sysdig-cli-scanner --apiurl $sysdig_api_url --policy john_policy --console-log 992382711296.dkr.ecr.us-east-1.amazonaws.com/ubuntu:22.04
+SECURE_API_TOKEN=$sysdig_api ./sysdig-cli-scanner --apiurl $sysdig_api_url --policy john_policy --console-log 992382711296.dkr.ecr.us-east-1.amazonaws.com/ubuntu_base:22.04
